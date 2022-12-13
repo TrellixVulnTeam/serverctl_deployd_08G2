558decfe0e0c98bdbd9dc71a1f01794d67964e01
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-13 12:32:46 +0000

    Adding tarfile member sanitization to extractall()

--- tests/routers/test_config.py
+++ tests/routers/test_config.py
@@ -203,7 +203,26 @@ def test_get_tar_archive() -> None:
     backup_path = Path("tests/fakes/backup_conf")
     backup_path.mkdir(exist_ok=True)
     with tarfile.open("tests/fakes/mock_conf.tar.gz", "r|gz") as tar_file:
-        tar_file.extractall(path=backup_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar_file, path=backup_path)
     _, mismatch, error = filecmp.cmpfiles(
         backup_path,
         MOCK_CONF_DIRPATH,
